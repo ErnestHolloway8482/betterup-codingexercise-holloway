359621fead2eea22a281ce729cb12acb9c9c2dc2
Ernest Holloway <ernest.holloway@embersoftwarellc.com>
2018-10-17 23:47:05 -0500

    Nuked hardcode credentiakls.

--- app/src/main/java/com/betterup/codingexercise/models/viewmodels/LoginVM.java
+++ app/src/main/java/com/betterup/codingexercise/models/viewmodels/LoginVM.java
@@ -47,10 +47,6 @@ public class LoginVM extends BaseVM {
         this.resourceManager = resourceManager;
 
         setupToolBar();
-
-        //For testing only please remove
-        username.set("ernest.holloway@embersoftwarellc.com");
-        password.set("Sprinter198!");
     }
 
     public void login() {
