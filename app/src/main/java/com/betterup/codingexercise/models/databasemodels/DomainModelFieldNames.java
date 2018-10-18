4adfe36e5252f784c6fa8e1cf2e82cbe6e800bd7
Ernest Holloway <ernest.holloway@embersoftwarellc.com>
2018-10-18 03:18:13 -0500

    Updated comments in the code.

--- app/src/main/java/com/betterup/codingexercise/models/databasemodels/DomainModelFieldNames.java
+++ app/src/main/java/com/betterup/codingexercise/models/databasemodels/DomainModelFieldNames.java
@@ -1,7 +1,7 @@
 package com.betterup.codingexercise.models.databasemodels;
 
 /**
- * This enumerator class helps to define a clean list of constants that are utilized by the {@link com.underarmour.android.mmf.dao.RealmAbstractDAO} class when it comes
+ * This enumerator class helps to define a clean list of constants that are utilized by the {@link com.betterup.codingexercise.daos.RealmAbstractDAO} class when it comes
  * to searching for items within the database that have common field names which are typically primary keys (uuid).
  */
 public enum DomainModelFieldNames {
--- app/src/main/java/com/betterup/codingexercise/views/Screen.java
+++ app/src/main/java/com/betterup/codingexercise/views/Screen.java
@@ -3,7 +3,7 @@ package com.betterup.codingexercise.views;
 import android.arch.lifecycle.ViewModel;
 
 /**
- * This interface is what every screen that is to be displayed via the {@link qv21.codingexercise.managers.NavigationManager} should implement.
+ * This interface is what every screen that is to be displayed via the {@link com.betterup.codingexercise.managers.NavigationManager} should implement.
  *
  * @param <VM>
  */
@@ -16,7 +16,7 @@ public interface Screen<VM extends ViewModel> {
     void setViewModel(final VM model);
 
     /**
-     * Configures the top level {@link android.support.v7.widget.Toolbar} for a given screen as defined by the {@link qv21.codingexercise.activities.MainActivity}
+     * Configures the top level {@link android.support.v7.widget.Toolbar} for a given screen as defined by the {@link com.betterup.codingexercise.activities.MainActivity}
      */
     void setupToolbar();
 }
